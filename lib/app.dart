e2d9074a3394767b9a394cbf7acfca9dbf1606e4
Haruto Muraki <114408959+Mgtantheta@users.noreply.github.com>
2024-07-21 21:35:45 +0900

    viewの実装をしてappとmainを分けた

---
+++ lib/app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'screens/pokemon_list_screen.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PokéDex',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const PokemonListScreen(),
+    );
+  }
+}
